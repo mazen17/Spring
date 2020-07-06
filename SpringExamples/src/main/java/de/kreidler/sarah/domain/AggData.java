f84ae44d7aff3d8d97ad8027e0c1fc3ca2f9ba18
mazen17 <mazen17@github.com>
2020-07-06 23:33:08 +0200

    add sarahs  code

--- SpringExamples/src/main/java/de/kreidler/sarah/domain/AggData.java
+++ SpringExamples/src/main/java/de/kreidler/sarah/domain/AggData.java
@@ -1,7 +1,29 @@
 package de.kreidler.sarah.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 public class AggData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private String tradeid;
+    private String isin;
+    private double mean;
+    //Sarah: noch nicht vollständig zunächst auf Mean beschränkt
+
+    public AggData() {
+    }
+
+    public AggData(String tradeid, String isin, double mean) {
+        this.tradeid = tradeid;
+        this.isin = isin;
+        this.mean = mean;
+    }
 }
